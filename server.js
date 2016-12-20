e452a117d24fefe7147790f6a43124775739ffd3
long phan <lphan1989@gmail.com>
2016-12-20 17:11:22 -0500

    server thangs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express")
 var app = express()
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3001
+
+app.use("/", express.static("public"));
 
 
 app.get("/", function(req,res) {
-  res.sendFile("index.html")
+  res.sendFile(__dirname + "/clock.html")
 })
 app.listen(PORT)
